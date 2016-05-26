66e5ea98f8d5ecdcb3ba1e8e8edddfef36e667ca
Danny Willems <contact@danny-willems.be>
2016-05-27 01:07:26 +0200

    Use cordova plugin for event listener

--- app/ml/test.ml
+++ app/ml/test.ml
@@ -1,7 +1,6 @@
 let on_device_ready _ =
   let c = Cordova_compass.t () in
-  Js._false
+  ()
 
 let _ =
-  Dom.addEventListener Dom_html.document (Dom.Event.make "deviceready")
-  (Dom_html.handler on_device_ready) Js._false
+  Cordova.Event.device_ready on_device_ready
